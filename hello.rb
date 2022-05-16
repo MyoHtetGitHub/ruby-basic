63205e1e451e4c704d44e531888cde2fd559b92c
MyoHtetGitHub <myohtetgithub1996@gmail.com>
2022-05-16 18:00:39 +0630

    about string in ruby

--- hello.rb
+++ hello.rb
@@ -7,3 +7,25 @@ def test (get_data)
 end
 
 test "How are you "
+
+# string concatenation test
+first_name = "Myo"
+last_name = "Htet"
+
+puts first_name + " " + last_name
+
+# string interpolation test
+
+puts "My first name is  #{first_name} and my last name is #{last_name}"
+
+# escaping
+puts 'Hey Jon \'How is  going\' i am fine'
+
+
+#get user input
+
+puts "Enter you name"
+
+input =gets.chomp
+
+puts "My name is #{input}"
--- note.md
+++ note.md
@@ -3,3 +3,9 @@
 ##  diffenence of print and put in ruby
     put has auto "\n" `next`line auto added
     print does not has next line  you anc added next line you can add `\n`
+
+
+### string interpolation 
+- > single quotes does not work in string interpolation so tha we can used double quotes
+
+  
